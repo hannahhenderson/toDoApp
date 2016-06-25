369d4df21f017263271645441cdab9bcb3785d16
Hack Reactor Students <accounts@hackreactor.com>
2016-06-25 11:33:43 -0700

    Pass all searchYouTube tests

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,50 +1,29 @@
 var searchYouTube = (options, callback) => {
-  console.log(options);
-  console.log(callback);
+  var obj = {
+    part: 'snippet',
+    type: 'video',
+    videoEmbeddable: 'true',
+    key: options.key,
+    q: options.query,
+    maxResults: options.max
+  };
 
   $.ajax({
-    cache: false,
-    data: $.extend({
-      key: options.key,
-      q: options.query,
-      part: 'snippet'
-    }, {maxResults: options.max, pageToken: $('#pageToken').val()}),
-    dataType: 'json',
-    type: 'GET',
-    timeout: 5000,
-    url: 'https://www.googleapis.com/youtube/v3/search'
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    TYPE: 'GET',
+    data: obj,
+    success: function(data) {
+      console.log(data.items);
+      callback(data.items);
+    },
+    error: function(data) {
+      console.log(data.items);
+    }
   });
-
-  // $.done(function(data) {
-  //   $('.btn-group').show();
-  //   if (typeof data.prevPageToken === "undefined") {
-  //     $("#pageTokenPrev").hide();
-  //   } else {
-  //     $("#pageTokenPrev").show();
-  //   }
-  //   if (typeof data.nextPageToken === "undefined") {
-  //     $("#pageTokenNext").hide();
-  //   } else {
-  //     $("#pageTokenNext").show();
-  //   }
-  //   var items = data.items, videoList = "";
-  //   $("#pageTokenNext").val(data.nextPageToken);
-  //   $("#pageTokenPrev").val(data.prevPageToken);
-  //   $.each(items, function(index,e) {
-  //     videoList = videoList + '<li class="hyv-video-list-item"><div class="hyv-content-wrapper"><a href="" class="hyv-content-link" title="'+e.snippet.title+'"><span class="title">'+e.snippet.title+'</span><span class="stat attribution">by <span>'+e.snippet.channelTitle+'</span></span></a></div><div class="hyv-thumb-wrapper"><a href="" class="hyv-thumb-link"><span class="hyv-simple-thumb-wrap"><img alt="'+e.snippet.title+'" src="'+e.snippet.thumbnails.default.url+'" width="120" height="90"></span></a></div></li>';
-  //   });
-  //   $("#hyv-watch-related").html(videoList);
-  //   // JSON Responce to display for user
-  //   new PrettyJSON.view.Node({
-  //     el:$(".hyv-watch-sidebar-body"), 
-  //     data:data
-  //   });
-  // });
 };
 
 window.searchYouTube = searchYouTube;
 
-
-// Sources: 
+// Source
 // Specific link: http://stackoverflow.com/questions/28018792/how-to-get-youtube-video-title-with-v3-url-api-in-javascript-w-ajax-json
 // Query: http://w3tweaks.com/web-api/21-youtube/45-search-youtube-videos-using-youtube-v3-api-using-jquery-ajax-call.html
