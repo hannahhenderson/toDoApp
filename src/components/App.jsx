7a7a6ad91069f5a62ada8c3be4a54b820545b626
Hack Reactor Students <accounts@hackreactor.com>
2016-06-24 20:36:32 -0700

    Commit prior to major changes, stuck passing onClick events
    Trying to feed them through all levels, it is not working at the moment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,82 @@
-var App = () => (
-  <div>
-  <h1>Hello, Hannah!</h1>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={exampleVideoData}/>
-    </div>
-  </div>
-);
+// var App = () => (
+//   <div>
+//   <h1>Hello, Hannah!</h1>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer video={exampleVideoData[0]}/>
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList videos={exampleVideoData}/>
+//     </div>
+//   </div>
+// );
 
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      curVideo: exampleVideoData[0]
+    };
+  }
+
+  onTitleClick (value) {
+    this.setState({
+      curVideo: value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.curVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={exampleVideoData} onTitleClick={this.onTitleClick}/>
+        </div>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+
+// class GroceryListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       done: false,
+//       boldHover: false
+//     };
+//   }
+
+//   onListItemClick() {
+//     this.setState({
+//       done: !this.state.done
+//     });
+//   }
+
+//   onListItemHover() {
+//     this.setState({
+//       boldHover: !this.state.boldHover
+//     })
+//   }
+
+//   render() {
+//     var style = {
+//       textDecoration: this.state.done ? 'line-through' : 'none',
+//       fontWeight: this.state.boldHover ? 'bold' : 'normal'
+//     };
+
+//     return (
+//       <li style={style} onClick={this.onListItemClick.bind(this)} onMouseOver={this.onListItemHover.bind(this)}>{this.props.groceries}</li>
+//     );
+//   }
+// }
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,7 +1,7 @@
 var VideoList = (props) => (
   <div className="video-list media">
-    {props.videos.map(video => 
-      <VideoListEntry video={video} />
+    {props.videos.map((video, i) => 
+      <VideoListEntry video={video} onTitleClick={props.onTitleClick}/>
     )}
   </div>
 );
@@ -16,53 +16,4 @@ VideoList.propTypes = {
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
 
-// ReactDOM.render(<VideoList videos={window.exampleVideoData} />, document.getElementById('app'));
-/*
-class GroceryListItem extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      done: false,
-      boldHover: false
-    };
-  }
-
-  onListItemClick() {
-    this.setState({
-      done: !this.state.done
-    });
-  }
-
-  onListItemHover() {
-    this.setState({
-      boldHover: !this.state.boldHover
-    })
-  }
-
-  render() {
-    var style = {
-      textDecoration: this.state.done ? 'line-through' : 'none',
-      fontWeight: this.state.boldHover ? 'bold' : 'normal'
-    };
-
-    return (
-      <li style={style} onClick={this.onListItemClick.bind(this)} onMouseOver={this.onListItemHover.bind(this)}>{this.props.groceries}</li>
-    );
-  }
-}
-
-var GroceryList = (props) => {
-  return (
-    <div>
-      <h2>My Todo List</h2>
-      <ul>
-        {props.groceries.map(groceries =>
-          <GroceryListItem groceries={groceries} />
-        )}
-      </ul>
-    </div>
-  )
-};
-ReactDOM.render(<GroceryList groceries={['hi', 'there', 'how', 'are', 'you']} />, document.getElementById("app"));
-*/
+// onTitleClick={props.onTitleClick}
